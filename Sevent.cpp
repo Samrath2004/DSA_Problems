faa091fff2277d6a6af9d24c74a5994275108c0c
Samrath Agarwal <113513345+Samrath2004@users.noreply.github.com>
2025-03-07 11:04:53 +0530

    Solved(2965.) Find Missing and Repeated Values on Leetcode.

--- Sevent.cpp
+++ Sevent.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int size = n * n;
+        vector<int> freq(size + 1, 0);
+        int repeating = -1, missing = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                freq[grid[i][j]]++;
+            }
+        }
+        for (int i = 1; i <= size; i++) {
+            if (freq[i] == 2) repeating = i;
+            if (freq[i] == 0) missing = i;
+        }      
+        return {repeating, missing};
+    }
+};
 
